5ef43a3992e7a542059785eb61c86fce747b6995
Ronnyriyana <riyanaronny@gmail.com>
2020-10-31 15:08:55 +0700

    bug favicon

--- application/views/kerangka/kerangka.php
+++ application/views/kerangka/kerangka.php
@@ -8,7 +8,7 @@
         <meta content="Coderthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/favicon.ico">
 
         <!-- App css -->
         <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bootstrap-stylesheet" />
