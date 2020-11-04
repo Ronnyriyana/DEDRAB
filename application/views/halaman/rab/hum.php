b8b3b9d850768b4c3e0b375f46f1f775422c9ef2
Ronnyriyana <riyanaronny@gmail.com>
2020-11-04 19:53:47 +0700

    oprek tab

--- application/views/halaman/rab/hum.php
+++ application/views/halaman/rab/hum.php
@@ -9,7 +9,6 @@
                     <li class="breadcrumb-item active"><a href="javascript: void(0);">RAB</a></li>
                 </ol>
             </div>
-            <h4 class="page-title">RAB</i></h4>
         </div>
     </div>
 </div>
@@ -22,7 +21,7 @@
 			<ul class="nav nav-pills" id="myTabalt" role="tablist">
 				<li class="nav-item">
 					<a class="nav-link active" id="ded-tab1" data-toggle="tab" href="#ded"
-						role="tab" aria-controls="rab" aria-expanded="true">DED</a>
+						role="tab" aria-controls="ded" aria-expanded="true">DED</a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" id="rab-tab1" data-toggle="tab" href="#rab"
@@ -30,32 +29,54 @@
 				</li>
 			</ul>
 			<div class="tab-content text-muted" id="myTabaltContent">
-				<div role="tabpanel" class="tab-pane fade in active show" id="ded" aria-labelledby="home-tab">
+				<div role="tabpanel" class="tab-pane fade in active show" id="ded" aria-labelledby="ded-tab">
 					<p>DED</p>
 				</div>
-				<div class="tab-pane fade" id="rab" role="tabpanel" aria-labelledby="profile-tab">
+				<div class="tab-pane fade" id="rab" role="tabpanel" aria-labelledby="rab-tab">
 					<!--RAB-->
 					<div class="col-12">
-						<div class="mt-4">
-							<ul class="nav nav-pills" id="myTabalt" role="tablist">
+							<!--Link-->
+							<ul class="nav nav-tabs" id="myTabalt" role="tablist">
 								<li class="nav-item">
-									<a class="nav-link active" id="ded-tab1" data-toggle="tab" href="#ded"
-										role="tab" aria-controls="rab" aria-expanded="true">DED</a>
+									<a class="nav-link active" id="hm-tab1" data-toggle="tab" href="#hm"
+										role="tab" aria-controls="hm" aria-expanded="true">Harga Material</a>
 								</li>
 								<li class="nav-item">
-									<a class="nav-link" id="rab-tab1" data-toggle="tab" href="#rab"
-										role="tab" aria-controls="rab">RAB</a>
+									<a class="nav-link" id="ahsp-tab1" data-toggle="tab" href="#ahsp"
+										role="tab" aria-controls="ahsp">AHSP</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" id="hsp-tab1" data-toggle="tab" href="#hsp"
+										role="tab" aria-controls="hsp">HSP</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" id="rekapitulasi-tab1" data-toggle="tab" href="#rekapitulasi"
+										role="tab" aria-controls="rekapitulasi">Rekapitulasi</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" id="boq-tab1" data-toggle="tab" href="#boq"
+										role="tab" aria-controls="boq">BOQ</a>
 								</li>
 							</ul>
+							<!--endLink-->
 							<div class="tab-content text-muted" id="myTabaltContent">
-								<div role="tabpanel" class="tab-pane fade in active show" id="ded" aria-labelledby="home-tab">
-									<p>DED</p>
+								<div role="tabpanel" class="tab-pane fade in active show" id="hm" aria-labelledby="HM-tab">
+									<p>Harga Material</p>
 								</div>
-								<div class="tab-pane fade" id="rab" role="tabpanel" aria-labelledby="profile-tab">
-									asdf
+								<div class="tab-pane fade" id="ahsp" role="tabpanel" aria-labelledby="ahsp-tab">
+									AHSP
+								</div>
+								<div class="tab-pane fade" id="hsp" role="tabpanel" aria-labelledby="hsp-tab">
+									HSP
+								</div>
+								<div class="tab-pane fade" id="rekapitulasi" role="tabpanel" aria-labelledby="rekapitulasi-tab">
+									Rekapitulasi
+								</div>
+								<div class="tab-pane fade" id="boq" role="tabpanel" aria-labelledby="boq-tab">
+									BOQ
 								</div>
 							</div>
-						</div>
+							<!--endContent-->
 					</div>
 					<!--endRAB-->
 				</div>
