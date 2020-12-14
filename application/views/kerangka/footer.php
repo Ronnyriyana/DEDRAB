f803f93d6cf1072b9614a846c73b683df316e790
Ronnyriyana <riyanaronny@gmail.com>
2020-12-14 22:22:14 +0700

    footer text

--- application/views/kerangka/footer.php
+++ application/views/kerangka/footer.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                2020 &copy; DED & RAB
+                2020 &copy; Dulurdesa.id
             </div>
         </div>
     </div>
