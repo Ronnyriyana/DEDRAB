1d527251ac01119b961f329efa67c6cae97ca678
Ronnyriyana <riyanaronny@gmail.com>
2021-01-05 20:56:42 +0700

    title

--- application/views/dashboard/kerangka/kerangka.php
+++ application/views/dashboard/kerangka/kerangka.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title>Starter Page | Uplon - Responsive Bootstrap 4 Admin Dashboard</title>
+        <title>Dashboard | <?= isset ($title_page)?$title_page:'DulurDesa' ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Responsive bootstrap 4 admin template" name="description" />
         <meta content="Coderthemes" name="author" />
