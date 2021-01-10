6a3c342f11cd8652d664232222d7c127c6d0723a
Ronnyriyana <riyanaronny@gmail.com>
2021-01-10 21:32:42 +0700

    Error 404

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,10 @@ class Home extends CI_Controller {
     public function __construct(){    
 		parent::__construct();
         $this->load->library("template");
-        $this->load->model("home_m");
+		$this->load->model("home_m");
+		if($this->session->has_userdata('user_logged')){
+			if($this->session->userdata('level')=="admin"){redirect('dashboard');}
+        }
     }
     
 	public function index()
---
+++ application/views/error_404.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Error 404 - Page Not Found | Dulurdesa.id</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Responsive bootstrap 4 admin template" name="description" />
+        <meta content="Coderthemes" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="<?= base_url();?>assets/images/favicon.ico">
+
+        <!-- App css -->
+        <link href="<?= base_url();?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bootstrap-stylesheet" />
+        <link href="<?= base_url();?>assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?= base_url();?>assets/css/app.min.css" rel="stylesheet" type="text/css"  id="app-stylesheet" />
+
+    </head>
+
+    <body class="authentication-bg">
+
+        <div class="account-pages pt-5 my-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6">
+                        <div class="text-center">
+                            <div class="text-error">4<i class="ion ion-md-sad mx-2"></i>4</div>
+                            <h3 class="text-uppercase text-white">Page not Found</h3>
+                            <p class="text-white mt-4">
+                                It's looking like you may have taken a wrong turn. Don't worry... it happens to
+                                the best of us. You might want to check your internet connection. Here's a little tip that might
+                                help you get back on track.
+                            </p>
+                            <br>
+                            <a class="btn btn-pink waves-effect waves-light" href="<?= site_url();?>"> Return Home</a>
+                        </div>
+
+                    </div> <!-- end col -->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end page -->
+
+        <!-- Vendor js -->
+        <script src="<?= base_url();?>assets/js/vendor.min.js"></script>
+
+        <!-- App js -->
+        <script src="<?= base_url();?>assets/js/app.min.js"></script>
+        
+    </body>
+</html>
